b771adb69b51f9e618818d40ce840d1c0e27085f
J.D Nicholls <jdnichollsc@hotmail.com>
2021-12-16 20:14:44 -0500

    Update TicketScreen.js

--- src/components/TicketScreen/TicketScreen.js
+++ src/components/TicketScreen/TicketScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { Select, MenuItem, Input } from "@material-ui/core";
 import {
   getArtists,
@@ -11,6 +11,31 @@ import "./TicketScreen.css";
 const TicketScreen = () => {
   const numberOfTickets = 2;
   const total = 2;
+  const [error, setError] = useState()
+  const [currentArtist, setCurrentArtist] = useState('')
+  const [currentConcert, setCurrentConcert] = useState('')
+  const [artists, setArtists] = useState([])
+  const [planNames, setPlanNames] = useState([])
+  const concerts = useMemo(() => {
+    const artist = artists.find((a) => a.name === currentArtist)
+    return artist?.concerts || [];
+  }, [currentArtist, artists])
+
+  const onLoad = useCallback(async () => {
+    try {
+      await Promise.all([
+        getArtists().then((artists) => setArtists(artists)),
+        getPlanNames().then((planNames) => setPlanNames(planNames)),
+      ])
+    }
+    catch (error) {
+      setError(error.message)
+    }
+  }, [])
+
+  useEffect(() => {
+    onLoad()
+  }, [])
   /*
     The goal here is to populate the first two dropdowns (Artist and Conerts)
     with the correct information.
@@ -18,28 +43,27 @@ const TicketScreen = () => {
 
     Time permitting, we can work on architecting solutions for the remainder of the dropdowns
   */
+
+  if (error) return (<label>{error}</label>)
+
   return (
     <div className="ticketScreenContainer">
       <div>
         <label> Artist </label>
-        <Select value={"Taylor Swift"}>
-          <MenuItem value={"Taylor Swift"}>Taylor Swift</MenuItem>
+        <Select value={currentArtist} onChange={(event) => setCurrentArtist(event.target.value)}>
+          {artists.map((artist) => <MenuItem key={artist.name} value={artist.name}>{artist.name}</MenuItem>)}
         </Select>
       </div>
       <div>
         <label> Concert </label>
-        <Select value="Taylor Swift June Tour">
-          <MenuItem value="Taylor Swift June Tour">
-            Taylor Swift June Tour
-          </MenuItem>
-          <MenuItem value="Taylor Swift July Tour">
-            Taylor Swift July Tour
-          </MenuItem>
+        <Select value={currentConcert} onChange={(event) => setCurrentConcert(event.target.value)}>
+          {concerts.map((concert) => <MenuItem key={concert.id} value={concert.id}>{concert.name}</MenuItem>)}
         </Select>
       </div>
       <div>
         <label> Ticket Type (Plan) </label>
-        <Select value="Basic">
+        <Select value={currentConcert} onChange={(event) => setCurrentConcert(event.target.value)}>
+          {Object.values(planNames).map((planName) => <MenuItem key={planName} value={planName}>{planName}</MenuItem>)}
           <MenuItem value="Basic">Basic</MenuItem>
         </Select>
       </div>
